b85603ed6324a3345efc41924fd663211634d51a
Idowu James <110604944+idowujames@users.noreply.github.com>
2022-10-31 18:34:39 +0100

    Issue #2 fix (#3)
    * Fixes #2 issue & minor code refactoring

--- download_folder_file_management.py
+++ download_folder_file_management.py
@@ -12,7 +12,7 @@ logging.basicConfig(
 os.chdir('C:/Users/DELL/Downloads')
 
 ## List of extensions for each file type
-software = ['.exe','.iso','msi']
+software = ['.exe','.iso','.msi']
 
 audio = [".3ga", ".aac", ".ac3", ".aif", ".aiff",
          ".alac", ".amr", ".ape", ".au", ".dss",
@@ -44,30 +44,43 @@ folders = ['Software','Audio','Video','Images','Documents','Zips','Jupyter_Noteb
 path = os.getcwd()
 
 
-## Function to create the extension directories if they don't already exist on system 
+ 
 def create_folders():
+    """
+        Function to create the extension directories if they don't already exist on system.
+
+        Note: If the folder is not created prior to moving of the files,
+        the files will be corrupted 
+
+    """
     for folder in folders:
         if not os.path.exists(f'{path}/{folder}'):
             os.makedirs(f'{path}/{folder}')
     return folder
 
-## Function to move file based on extension to their respective directories
 def file_type(file):
+    """
+        Function to move file based on extension to their respective directories.
+      
+        Using absolute path in shutil.move() to avoid error raised if the same file 
+        already exist in destination folder
+
+    """
     ext = os.path.splitext(file)[1]
     if ext in software:
-        shutil.move(file, f'{path}/Software')
+        shutil.move(os.path.join(path, file), os.path.join(f'{path}\Software', file))
     elif ext in video:
-        shutil.move(file, f'{path}/Video')
+        shutil.move(os.path.join(path, file), os.path.join(f'{path}\Video', file))
     elif ext in audio:
-        shutil.move(file, f'{path}/Audio')
+        shutil.move(os.path.join(path, file), os.path.join(f'{path}\Audio', file))
     elif ext in img:
-        shutil.move(file, f'{path}/Images')
-    elif ext in documents:
-        shutil.move(file, f'{path}/Documents')
+        shutil.move(os.path.join(path, file), os.path.join(f'{path}\Images', file))
+    if ext in documents:
+        shutil.move(os.path.join(path, file), os.path.join(f'{path}\Documents', file))
     elif ext in zips:
-        shutil.move(file, f'{path}/Zips')
+        shutil.move(os.path.join(path, file), os.path.join(f'{path}\Zips', file))
     elif ext in jupyter_notes:
-        shutil.move(file, f'{path}/Jupyter_Notebooks')
+        shutil.move(os.path.join(path, file), os.path.join(f'{path}\Jupyter_Notebooks', file))
         
 
 
